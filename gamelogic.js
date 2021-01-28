62250e0c9ff845fe01177ce3d31341372c18ab2a
Marijn Ploeg <marijnploeg@gmail.com>
2021-01-28 19:28:05 +0100

    Finished homework, all the tests work now :)

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,36 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  // Split the word into an array
+  let correctWord = word.split('');
+  // Create an empty string to later be filled
+  let galgjeWoord = '';
+
+  // Iterate over the letters
+  for (let index = 0; index < correctWord.length ; index++) {
+    const letterInWord = correctWord[index];
+    // Check if the guessed letters are in the word
+    const letterGuessed = guesses.includes(letterInWord);
+    if (!letterGuessed) {
+      galgjeWoord = galgjeWoord + '_ ';
+    }
+    if (letterGuessed) {
+      galgjeWoord = galgjeWoord + letterInWord + ' ';
+    }
+  }
+  return galgjeWoord;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  // isGameWon should return false when all letters have not been guessed yet
+  const wordSoFar = displayWordSoFar(word, guesses);
+  // Stich the letters together
+  let stichWord = wordSoFar.replace(/\s/g,'');
+  return stichWord === word;
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  // isGameLost should return true if you've guessed 7 wrong letters or more
+  let amountOfGuesses = guesses.length;
+  return amountOfGuesses >= 7;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -3,14 +3,32 @@ const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+  console.log(displayWordSoFar(word, guesses));
 
   const letter = question("Raad een letter: ");
 
-  // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+  if (isCharacterALetter(letter) && letter.length === 1) {
+    // voeg de geraden letter toe aan de array met guesses
+    guesses.push(letter);
 
-  // volgende ronde! we roepen game nog een keer aan
-  game(word, guesses);
+    // Check if the game is won or lost
+    if (isGameWon(word, guesses)) {
+      console.log('Congratulations, you have won')
+    }
+    else if (isGameLost(word, guesses)) {
+      console.log('You lost the game!')
+    }
+    // volgende ronde! we roepen game nog een keer aan
+    else {
+      game(word, guesses);
+    }
+  }
+  else  {
+    console.log('Please make sure you enter a single letter! ')
+    game(word, guesses)
+  }
+
+
 }
 
 console.log(`
@@ -24,3 +42,7 @@ __________
 `);
 
 game("javascript", []);
+
+function isCharacterALetter(char) {
+  return (/[a-zA-Z]/).test(char)
+}
